6ead0923337c293b416e16830749400c1e6ec420
894kun <heart_warming_movie@yahoo.co.jp>
2023-01-03 06:54:59 +0000

    タスク追加機能を実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import Style from "./styles.css";
 
 const App = () => {
+  const [taskInput, setTaskInput] = useState("");
+  const [compTodos, setCompTodos] = useState(["sample1", "sample2"]);
+  const [inCompTodos, setInCompTodos] = useState(["sample3"]);
+
+  const onChangeTaskInput = (event) => {
+    setTaskInput(event.target.value);
+  };
+
+  const onClickAdd = () => {
+    if (taskInput === "") return;
+    setCompTodos([...compTodos, taskInput]);
+    setTaskInput("");
+  };
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="Todoを入力" />
-        <button>追加</button>
+        <input
+          placeholder="Todoを入力"
+          onChange={onChangeTaskInput}
+          value={taskInput}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
-        　<p className="title">未完了のTodo</p>
+        <p className="title">未完了のTodo</p>
         <ul>
-          <div>
-            <li className="list-row">
-              <p>サンプル1</p>
-              <button>完了</button>
-              <button>削除</button>
-            </li>
-            <li className="list-row">
-              <p>サンプル2</p>
-              <button>完了</button>
-              <button>削除</button>
-            </li>
-          </div>
+          {compTodos.map((todo) => {
+            return (
+              <div key={todo}>
+                <li className="list-row">
+                  <p>{todo}</p>
+                  <button>完了</button>
+                  <button>削除</button>
+                </li>
+              </div>
+            );
+          })}
         </ul>
       </div>
       <div className="complete-area">
-        　<p className="title">完了したTodo</p>
+        <p className="title">完了したTodo</p>
         <ul>
-          <div>
-            <li className="list-row">
-              <p>サンプル3</p>
-              <button>戻す</button>
-            </li>
-          </div>
+          {inCompTodos.map((todo) => {
+            return (
+              <div key={todo}>
+                <li className="list-row">
+                  <p>{todo}</p>
+                  <button>戻す</button>
+                </li>
+              </div>
+            );
+          })}
         </ul>
       </div>
     </>
